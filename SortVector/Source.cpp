3b1feb52d5754e6c23b88b60dda3560885461678
Alina <klishevaad@gmail.com>
2024-02-21 14:52:39 +0300

    task num. 4 added. check for array size added

--- SortVector/Source.cpp
+++ SortVector/Source.cpp
@@ -1,35 +1,54 @@
 #include<iostream>
-#include <vector>
+#include<vector>
+#include<fstream>
+
+std::vector<int> readVectorFromFile(std::string fileName) {
+	std::ifstream file("input.txt");
+	if (!file.is_open()) {
+		std::cout << "Can not open the file" << std::endl;
+		std::vector<int> vec;
+		return vec;
+	}
+	std::vector<int> vec{ std::istream_iterator<int>(file), std::istream_iterator<int>() };
+	file.close();
+	return vec;
+}
 
 void insertionSortSquareBrakets(std::vector<int> & arr) {
-	for (int i = 1; i < arr.size(); i++) {
-		int j = i;
-		while (j > 0 && arr[j] < arr[j - 1]) {
-			std::swap(arr[j], arr[j - 1]);
-			j--;
+	if (arr.size() > 1) {
+		for (int i = 1; i < arr.size(); i++) {
+			int j = i;
+			while (j > 0 && arr[j] < arr[j - 1]) {
+				std::swap(arr[j], arr[j - 1]);
+				j--;
+			}
 		}
 	}
 }
 
 void insertionSortAtOperator(std::vector<int>& arr) {
-	for (int i = 1; i < arr.size(); i++) {
-		int j = i;
-		while (j > 0 && arr.at(j) < arr.at(j - 1)) {
-			std::swap(arr.at(j), arr.at(j - 1));
-			j--;
+	if (arr.size() > 1) {
+		for (int i = 1; i < arr.size(); i++) {
+			int j = i;
+			while (j > 0 && arr.at(j) < arr.at(j - 1)) {
+				std::swap(arr.at(j), arr.at(j - 1));
+				j--;
+			}
 		}
 	}
 }
 
 
 void insertionSortIterator(std::vector<int>& arr) {
-	std::vector<int>::iterator current = arr.begin() + 1;
-	std::vector<int>::iterator temp;
-	for (current; current < arr.end(); current.operator++()) {
-		temp = current;
-		while (temp > arr.begin() && *(temp - 1) > *temp) {
-			std::swap(*temp, *(temp - 1));
-			temp.operator--();
+	if (arr.size() > 1) {
+		std::vector<int>::iterator current = arr.begin() + 1;
+		std::vector<int>::iterator temp;
+		for (current; current < arr.end(); current.operator++()) {
+			temp = current;
+			while (temp > arr.begin() && *(temp - 1) > *temp) {
+				std::swap(*temp, *(temp - 1));
+				temp.operator--();
+			}
 		}
 	}
 }
@@ -43,10 +62,12 @@ void printVector(const std::vector<int>& arr) {
 		std::cout << std::endl;
 }
 
+
 int main(int argc, char* argv[]) {
+	std::string inputFile = "input.txt";
 	std::vector<int> arr1 = {12, 4, -5, 1, 0};
 	std::vector<int> arr2 = arr1;
-	std::vector<int> arr3 = arr1;
+	std::vector<int> arr3 = readVectorFromFile(inputFile);
 	insertionSortSquareBrakets(arr1);
 	insertionSortAtOperator(arr2);
 	insertionSortIterator(arr3);
